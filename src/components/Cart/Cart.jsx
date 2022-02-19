b63608f5bf1eedfaac47de059683a9434f594d43
InsideAmber <lostinthoughtshard@gmail.com>
2022-02-19 15:03:34 +0530

    first commit

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Container, Typography, Button, Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import CartItem from './CartItem/CartItem';
+import useStyles from './styles';
+
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
+  const classes = useStyles();
+
+  const EmptyCart = () => (
+    <Typography variant='subtitle1'>
+      You have no items in your shopping cart,
+      <Link to='/' className={classes.link}>
+        start adding some
+      </Link>
+      !
+    </Typography>
+  );
+
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={4}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={6} lg={3} key={item.id}>
+            <CartItem
+              item={item}
+              onUpdateCartQty={handleUpdateCartQty}
+              onRemoveFromCart={handleRemoveFromCart}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant='h4'>
+          Subtotal : {cart.subtotal.formatted_with_symbol}
+        </Typography>
+        <div>
+          <Button
+            color='secondary'
+            className={classes.emptyButton}
+            size='large'
+            variant='contained'
+            type='button'
+            onClick={handleEmptyCart}
+          >
+            Empty Cart
+          </Button>
+          <Button
+            component={Link}
+            to='/checkout'
+            color='primary'
+            className={classes.checkoutButton}
+            size='large'
+            variant='contained'
+            type='button'
+          >
+            Checkout
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+
+  if (!cart.line_items) return 'Loading...';
+
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant='h4' gutterBottom>
+        Your Shopping Cart
+      </Typography>
+      {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
+
+export default Cart;
